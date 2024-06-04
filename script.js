532bfb726d6205201c18a3d67dd5a07c4898399a
Jordin Gutierrez <sjgutierrez2016@gmail.com>
2024-06-04 12:04:48 -0500

    Ejercicio #3 javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+let a_Nacimiento;
+let a_actual;
+let edad; 
+
+a_Nacimiento = Number (prompt (`ingrese año de nacimiento`));
+a_Edad_actual = Number (prompt(`ingrese edad actual`));
+
+edad= a_actual - a_Nacimiento;
+
+document.write(`su edad es: ` + edad)
